556416845edb3a9bcdadcb4e1429c44f46d7240e
Minyoung Seol <seol.ca06@gmail.com>
2022-11-24 20:59:00 -0500

    added mock incident data

--- teamproject/mock.incidents.ts
+++ teamproject/mock.incidents.ts
@@ -4,13 +4,20 @@ export const INCIDENTS: Incident[] = [
     {
         caseNo: 1,
         category: "Technical",
-        created: "24th Nov, 2022",
-        updated: " - ",
-        status: "Submitted"
+        created: "22th Nov, 2022",
+        updated: " 23th Nov, 2022 ",
+        status: "Completed"
     },
     {
         caseNo: 2,
         category: "Customer Service",
+        created: "23th Nov, 2022",
+        updated: " 23th Nov, 2022 ",
+        status: "In Progress"
+    },
+    {
+        caseNo: 3,
+        category: "Human Resource",
         created: "24th Nov, 2022",
         updated: " - ",
         status: "Submitted"
