7eca593ea487aeae78ca774d32cda2e7e3e92fef
Minyoung Seol <seol.ca06@gmail.com>
2022-11-28 14:44:44 -0500

    included services folder

--- teamproject/src/app/app.module.ts
+++ teamproject/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
---
+++ teamproject/src/app/services/incidents.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Incident } from 'Incident';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncidentsService {
+
+  private url = 'http://localhost:3201/api/get-incidents'
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Incident[]> {
+    return this.http.get<Incident[]>(this.url);
+  }
+}
